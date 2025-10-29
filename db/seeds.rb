65df0d7472858982f8a13506583c07df2baf2a29
Tin Tran <thuutin@gmail.com>
2025-10-29 23:33:20 +0700

    Restore db if not exists

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,9 @@
 
 puts 'Run the following command to create demo data: `rake demo_data:default`' if Rails.env.development?
 
-ActiveRecord::Base.connection.execute("PRAGMA foreign_keys = OFF;")
 ActiveRecord::Base.transaction do
   Dir[Rails.root.join('db', 'seeds', '*.rb')].sort.each do |file|
     puts "Loading seed file: #{File.basename(file)}"
-    if File.basename(file) == "db_restore.rb" && Family.count > 0
-      puts "Skipping db_restore.rb because data already exists"
-      next
-    end
     require file
   end
 end
-ActiveRecord::Base.connection.execute("PRAGMA foreign_keys = ON;")
